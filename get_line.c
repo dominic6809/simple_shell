4b2f35e5d7e10ba3452fcd6de953f5ceba45ceb4
dominic6809 <muuomusyoki018@gmail.com>
2023-05-19 04:10:18 -0700

    updated

---
+++ get_line.c
@@ -0,0 +1,77 @@
+#include "shell.h"
+
+/**
+ * get_input - reads input from STDIN and stores it in info_t struct
+ * @info: pointer to info_t struct.
+ *
+ * Returns: number of chars read or (-1) incase of an error.
+ */
+ssize_t get_input(info_t *info)
+{
+	ssize_t chars_read;
+	size_t input_size = 0;
+	char *input_buffer = Null;
+
+	/* read input from STDIN using getline */
+	chars_read = _getline(info, &input_buffer, &input_size);
+	if (chars_read == -1)
+		return (-1);
+	/* store input in info_t struct */
+	info->input = input_buffer;
+	info->input_len = (size_t)chars_read;
+
+	return (chars_read);
+}
+/**
+ * _getline - reads a line from STDIN.
+ * @info: pointer to info_t struct.
+ * @buffer: pointer to buffer to store line
+ * @buf_size: pointer to size of buffer.
+ *
+ * Return: number of characters read or -1 incase error
+ */
+int _getline(info_t *info, char **buffer, size_t *buf_size)
+{
+	int chars_read;
+
+	/* handle SIGINT signal */
+	signal(SIGINT, sigintHandler);
+
+	/* read input from STDIN using getline */
+	chars_read = getline(buffer, buf_size, stdin);
+	if (chars_read == -1)
+	{
+		/* handle EOF */
+		if (feof(stdin))
+		{
+			_puts("\n");
+			ffree(buffer);
+			return (-1);
+		}
+		perror("getline");
+		ffree(buffer);
+		return (-1);
+	}
+	/* remove trailing newline char */
+	if ((*buffer)[chars_read - 1] == '\n')
+		(*buffer)[chars_read - 1] = '\0';
+	if (_strlen(*buffer) == 0)
+	{
+		ffree(buffer);
+		return (-1);
+	}
+	return (chars_read);
+}
+/**
+ * sigintHandler - handles SIGINT signal.
+ * @sig_num: signal number
+ */
+void sigintHandler(int sig_num)
+{
+	if (sig_num == SIGINT)
+	{
+		_puts("\n");
+		_puts(PROMPT);
+		fflush(stdout);
+	}
+}
