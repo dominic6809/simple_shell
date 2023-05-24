126726a58f628cb3d0f92f7a51d831105bac0cee
dominic6809 <muuomusyoki018@gmail.com>
2023-05-23 23:14:34 -0700

    updated

--- info.c
+++ info.c
@@ -7,27 +7,34 @@
 
 void clear_info(info_t *info)
 {
-	ffree(info->av);
-	ffree(char *);
-	ffree(list_t *);
+	if (!info)
+		return;
+
+	info->argv = NULL;
+	info->argc = 0;
+	info->environ = NULL;
 }
-
 /**
- * set_info - initializes info_t struct with provided arguments
- * @info: pointer to info_t struct to initialize..
- * @av: arr of arguments to set as info->av
+ * set_info - initializes info_t struct with the provided arguments.
+ * @info: a pointer to info_t struct to initialize.
+ * @av: array of arguments to set as info->av.
  */
-
 void set_info(info_t *info, char **av)
 {
-	info->av = av;
-	info->path = strtow(get_env_value(info, "PATH"), ":");
-	info->env = (char **)environ;
+	if (!info == NULL || av == NULL)
+		return;
+
+	info->argv = av;
+	info->argc = 0;
+
+	while
+		(av[info->argc]);
+	info->argc++;
 }
 /**
- * free_info - frees all memory used by an info_t struct
+ * free_info - it frees all the memmory used by an info_t struct.
  * @info: pointer to info_t struct to free.
- * @exit_code: exit code to pass to exit()
+ * @exit_code: exits the code to pass to exit()
  */
 void free_info(info_t *info, int exit_code)
 {
