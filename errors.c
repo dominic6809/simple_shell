2370398de8b106775002db2308d26ef872e82d2f
dominic6809 <muuomusyoki018@gmail.com>
2023-05-24 16:13:14 -0700

    updated

--- errors.c
+++ errors.c
@@ -22,11 +22,11 @@ void _eputs(char *str)
 int _eputchar(char c)
 {
 	static char buff[WRITE_BUF_SIZE];
-	int i;
+	static int i;
 
-	if (c == BUF_FLUSH || WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		write(2, buff, i);
+		write(2, buff,  i);
 		i = 0;
 	}
 	if (c != BUF_FLUSH)
