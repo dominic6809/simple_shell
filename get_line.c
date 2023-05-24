b4a9abf7ca5398cfb36c0b37d4865163a495ac71
dominic6809 <muuomusyoki018@gmail.com>
2023-05-24 07:42:25 -0700

    updated

---
+++ get_line.c
@@ -0,0 +1,69 @@
+#include "shell.h"
+
+/**
+ * get_input - reads input from STDIN and stores it in info_t struct.
+ * @info: pointer to info_t struct.
+ * Return: number of chars read or (-1) incase of an error occurrence.
+ */
+ssize_t get_input(info_t *info)
+{
+	ssize_t chars_read;
+
+	if (info == NULL)
+		return (-1);
+	/* read input from STDIN using getline */
+	if (chars_read == -1)
+		perror("_getline");
+	return (chars_read);
+}
+/**
+ * _getline - reads a line from STDIN
+ * @info: pointer to info_t struct.
+ * @buffer: pointer to buffer to store line.
+ * @buf_size: pointer to size of buffer.
+ * Retun: number of chars read or -1 if error occurs.
+ */
+int _getline(info_t *info, char **buffer, size_t *buf_size)
+{
+	int chars_read;
+
+	/* read input from STIN using getline */
+
+	if (chars_read == -1)
+	{
+		/* handle EOF */
+		if (feof(stdin))
+		{
+			_puts("\n");
+			ffree(buffer);
+			return (-1);
+		}
+		perror("getline");
+		ffree(buffer);
+		return (-1);
+	}
+	/* remove trailing newline whitespace */
+
+	if ((*buffer)[chars_read - 1] == '\n')
+		(*buffer)[chars_read - 1] = '\0';
+	/* check if input is empty */
+	if (_strlen(*buffer) == 0)
+	{
+		ffree(buffer);
+		return (-1);
+	}
+	return (chars_read);
+}
+/**
+ * sigintHandler - handles SIGINT signal.
+ * @sig_num: signal number.
+ */
+void sigintHandler(int sig_num)
+{
+	if (sig_num == SIGINT)
+	{
+		_puts("\n");
+		_puts("$");
+		fflush(stdout);
+	}
+}
