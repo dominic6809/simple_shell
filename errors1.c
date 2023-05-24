acee245d5d23ff5d165e109e056578a47c362c42
dominic6809 <muuomusyoki018@gmail.com>
2023-05-24 06:52:09 -0700

    updated

---
+++ errors1.c
@@ -0,0 +1,124 @@
+#include "shell.h"
+
+/**
+ * erratoi - converts a string to an integer.
+ * @str: string to be changed.
+ * Return: integer value of the string.
+ */
+int erratoi(char *str)
+{
+	int i, result;
+
+	result = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			result = result * 10 + (str[i] - '0');
+		else
+		{
+			_puts("error: invalid argument\n");
+			return (-1);
+		}
+	}
+	return (result);
+}
+/**
+ * print_error - outputs an error
+ * @info: a ponter to the info_t struct.
+ * @msg: error message to print.
+ */
+void print_error(info_t *info, char *msg)
+{
+	_putsfd(info->fname, 2);
+	_putsfd(":", 2);
+	_putsfd(msg, 2);
+}
+/**
+ * print_d - outputs an integer to the file descriptor
+ * @input: the integer to print.
+ * @fd: the file descriptor to write.
+ * Return: number of chars written, -1 on error.
+ */
+int print_d(int input, int fd)
+{
+	int i;
+	int len = 0;
+	int count = 0;
+	char buff[20];
+
+	if (input < 0)
+	{
+		input = -input;
+		count += write(fd, "0", 1);
+	}
+	if (input == 0)
+	{
+		count += write(fd, "0", 1);
+		return (count);
+	}
+	while
+		(input > 0)
+		{
+			buff[len++] = '0' + (input % 10);
+			input /= 10;
+		}
+	for (i = len - 1; i >= 0; i--)
+	{
+		count += write(fd, &buff[i], 1);
+	}
+	return (count);
+}
+/**
+ * convert_number - changes a number to a string with a given base.
+ * @num: the number to convert.
+ * @base: base to convert to.
+ * @uppercase: whether to use uppercase or lowercase letters.
+ * Return: pointer to the string representation of the number.
+ */
+char *convert_number(long int num, int base, int uppercase)
+{
+	static char buffer[50];
+	char *ptr;
+	char hex_digits[] = "0123456789abcdef";
+	char HEX_DIGITS[] = "0123456789ABCDEF";
+
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do {
+		if (uppercase)
+			*--ptr = HEX_DIGITS[num % base];
+		else
+			*--ptr = hex_digits[num % base];
+		num /= base;
+	} while (num != 0);
+	return (ptr);
+}
+/**
+ * remove_comments - ommits comments from a string.
+ * @str: the string to remove comments from.
+ */
+void remove_comments(char *str)
+{
+	int i, in_comment, len;
+
+	len = _strlen(str);
+	in_comment = 0;
+
+	for (i = 0; i < len; i++)
+	{
+		if (in_comment)
+		{
+			if (str[i] == '\n')
+				in_comment = 0;
+			else
+				str[i] = '#';
+		}
+		else
+		{
+			if (str[i] == '#')
+				in_comment = 1;
+		}
+	}
+}
