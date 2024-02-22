6d93daa8a5addcafd9e210abb3cb0e7be16c4a2a
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-02-22 11:37:28 +0300

    Update exec.c

--- exec.c
+++ exec.c
@@ -1,27 +1,18 @@
 #include "shell.h"
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
 
-#define MAX_ARGS 64
-#define MAX_ARG_LENGTH 64
-
-char *program_name;
-
+/**
+ * execute - Execute a command with arguments
+ * @args: Array of strings containing command and arguments
+ * Return: Status code of the command execution
+ */
 int execute(char **args);
 int execute_exit(char **args);
 int execute_env(void);
 int execute_command(char **args);
 
-/**
- * main - Entry point of the shell program
- * @argc: Number of arguments passed to the program
- * @argv: Array of arguments passed to the program
- * Return: Always 0
- */
 int main(int argc, char **argv)
 {
+    char *program_name;
     program_name = strdup(argv[0]);
     char input[MAX_ARGS][MAX_ARG_LENGTH];
     char *args[MAX_ARGS];
@@ -66,7 +57,6 @@ int main(int argc, char **argv)
     free(program_name);
     return 0;
 }
-
 /**
  * execute - Execute command
  * @args: Array of strings containing command and arguments
@@ -74,60 +64,110 @@ int main(int argc, char **argv)
  */
 int execute(char **args)
 {
-    pid_t pid = fork();
-    int status;
-
-    if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else if (pid < 0)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        do
-        {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-
-    return 1;
+	if (args[0] == NULL)
+	{
+		return (1);
+	}
+	else if (_strcmp(args[0], "exit") == 0)
+	{
+		return (execute_exit(args));
+	}
+	else if (_strcmp(args[0], "env") == 0)
+	{
+		return (execute_env());
+	}
+	else
+	{
+		return (execute_command(args));
+	}
 }
 
 /**
  * execute_exit - Handle the "exit" command
+ *
  * @args: Array of strings containing command and arguments
- * Return: Void
+ *
+ * Return: Status code of the command execution
  */
 int execute_exit(char **args)
 {
-    if (args[1] != NULL)
-    {
-        exit(atoi(args[1]));
-    }
-    else
-    {
-        exit(EXIT_SUCCESS);
-    }
+	if (args[1] != NULL)
+	{
+		int status = _atoi(args[1]);
+
+		exit(status);
+	}
+	else
+	{
+		exit(EXIT_SUCCESS);
+	}
 }
 
 /**
  * execute_env - Handle the "env" command
- * Return: Always 1
+ *
+ * Return: Status code of the command execution
  */
 int execute_env(void)
 {
-    extern char **environ;
-    for (char **env = environ; *env != NULL; env++)
-    {
-        shell_print(*env);
-    }
-    return 1;
+	char **env = __environ;
+
+	while (*env != NULL)
+	{
+		_fputs(*env, stdout);
+		_fputc('\n', stdout);
+		env++;
+	}
+	return (1);
+}
+
+/**
+ * execute_command - Execute a command
+ *
+ * @args: Array of strings containing command and arguments
+ *
+ * Return: Status code of the command execution
+ */
+int execute_command(char **args)
+{
+	int status;
+	char *command_path = get_command_path(args[0]);
+
+	if (command_path != NULL)
+	{
+		pid_t child_pid = fork();
+
+		if (child_pid == 0)
+		{
+			if (execve(command_path, args, environ) == -1)
+			{
+				perror("execve");
+				exit(-1);
+			}
+		}
+		else if (child_pid < 0)
+		{
+			perror("fork");
+			exit(-1);
+		}
+		else
+		{
+			wait(&status);
+			free(command_path);
+			if (WIFEXITED(status))
+				status = WEXITSTATUS(status);
+			if (!isatty(STDIN_FILENO))
+				return (status);
+		}
+	}
+	else
+	{
+		_fputs(program_name, stdout);
+		_fputs(": 1: ", stdout);
+		_fputs(args[0], stdout);
+		_fputs(": not found", stdout);
+		_fputc('\n', stdout);
+	}
+
+	return (0);
 }
